82682d48d1f63db4dad4e2eb19ad443a5db04dfa
Nagarajan Aadhi <127421404+ITSME1516@users.noreply.github.com>
2025-05-05 15:05:01 +0530

    Update 3_Predict.py

--- pages/3_Predict.py
+++ pages/3_Predict.py
@@ -10,7 +10,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer, WordNetLemmatizer
 from sklearn.linear_model import SGDClassifier
-
+nltk.download("all")
 # Load saved components
 lr = joblib.load('tag_predictor.pkl')
 tfidf = joblib.load('tfidf_vectorizer.pkl')
